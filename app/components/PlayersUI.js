f455e1d377f05250bd5929a674f777b3674fc763
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-04-09 21:06:04 +0200

    playerUI started

--- app/components/PlayersUI.js
+++ app/components/PlayersUI.js
@@ -1,16 +1,34 @@
 import { useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function PlayersUI() {
-  const paragraphStyles = `flex`;
+  const commonTextClasses = `px-1 bg-blue-300 text-sm rounded-md md:text-lg lg:text-2xl`;
+  const commonWrapperClasses = `absolute`;
+
+  const playerUI = [{ playerName: "Jan", textClasses: ``, wrapperClasses: `` }];
 
   return (
     <>
       <div className={`absolute left-0 top-0 h-full w-full`}>
-        <div className={``} id="player1">
-          <p className={``}>Jan</p>
+        <div
+          className={twMerge(
+            commonWrapperClasses,
+            `left-16 top-1 portrait:-top-3`,
+          )}
+          id="player1"
+        >
+          <p className={twMerge(commonTextClasses, `bg-stone-900 text-white`)}>
+            Jan
+          </p>
         </div>
-        <div className={``} id="player2">
-          <p className={``}>Carla</p>
+        <div
+          className={twMerge(
+            commonWrapperClasses,
+            `bottom-1 right-16 portrait:-bottom-3`,
+          )}
+          id="player2"
+        >
+          <p className={twMerge(commonTextClasses, `bg-white`)}>Carla</p>
         </div>
       </div>
     </>
